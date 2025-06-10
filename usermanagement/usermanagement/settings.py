c8040f6a07f43c0669491b06ad27bb7cde86ae7a
omkarawasthi <omkarawasthi67@gmail.com>
2025-06-10 19:42:36 +0530

    Create models, serializers, and view for CRUD operations

--- usermanagement/usermanagement/settings.py
+++ usermanagement/usermanagement/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-on*_^3uz#gkkxhz((vsrnj002y(dl$m0z)nk1zthxf(cf!4%zq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
